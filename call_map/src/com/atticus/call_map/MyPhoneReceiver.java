cded7ce855586364377731fef5683f03b8efd464
amaximus <atticus.rationalis@gmail.com>
2012-10-08 14:11:47 +0200

    Minor changes

--- call_map/src/com/atticus/call_map/MyPhoneReceiver.java
+++ call_map/src/com/atticus/call_map/MyPhoneReceiver.java
@@ -4,8 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
-import android.util.Log;
+//import android.util.Log;
 
 public class MyPhoneReceiver extends BroadcastReceiver {
 
